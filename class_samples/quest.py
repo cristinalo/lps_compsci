9cfd0b6bcef9c0fc687bb86b611667fec39375b6
Cristina Lopez <rc.lopez.cristina@leadps.org>
2016-11-09 17:40:54 +0000

    finished ps3

--- class_samples/quest.py
+++ class_samples/quest.py
@@ -18,8 +18,18 @@ fork = raw_input()
 
 if fork == 'right' and strength < 4:
 	print("Oh No! A boulder has squished you! Eat more vegetables to be stronger next time!")
-elif fork == 'right' and strength >= 5:
+elif fork == 'right' and strength > 5:
 	print("You were able to stop a boulder from crushing you thanks to those biceps!")
-if fork == 'right' and health <= 3:
-	print("Lol, dont even try to go this way with that health! You have died from a heart attack.")
+elif fork == 'right' and health <= 3:
+	print(str(name) + " dont even try to go this way with that health! You have died from a heart attack.")
+elif fork == 'right' and luck >= 6:
+	print("You got lucky enough and ran away before the zombies had a chance to get you but your partner Glenn Saldy died :(")
 
+if fork == 'left' and luck >= 5:
+	print("You got lucky! You didnt get bitten by a spider but now you wont be able to turn into spider man :(")
+elif fork == 'left' and luck <= 4:
+	print("You are so unlucky, you have died from tripping on your own feet by getting chased by a bee")
+elif fork == 'left' and health >= 5:
+	print("You have survived not dying from a heart attack when Trump was declared President!")
+elif fork == 'left' and strength >= 5:
+	print("You were strong enough to break up a fight, good job") 
